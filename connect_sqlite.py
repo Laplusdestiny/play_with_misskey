bd44370fe2bb1f1c62a0ab7a5deb3c043a415fbb
Laplusdestiny <prayonshootingstars@gmail.com>
2023-07-09 01:17:38 +0900

    Added function - Added function to add user into list - Renamed a script file name - Revised processes to add record into sqlite without duplicates

--- connect_sqlite.py
+++ connect_sqlite.py
@@ -7,3 +7,9 @@ import pandas as pd
 def save_to_db(db_path, df: pd.DataFrame, table: str, **kwargs):
     with sqlite3.connect(db_path) as conn:
         df.to_sql(table, conn, **kwargs)
+
+
+def get_data(db_path, query):
+    with sqlite3.connect(db_path) as conn:
+        data = pd.read_sql_query(query, conn)
+    return data
--- reaction.py
+++ reaction.py
@@ -8,7 +8,8 @@ from time import sleep
 import schedule
 import argparse
 import json
-from save_sqlite import save_to_db
+from connect_sqlite import save_to_db, get_data
+import datetime
 
 
 def read_config(path):
@@ -41,18 +42,25 @@ def get_note():
     }
     result2 = get_result(endpoint, params, misskeyio_config["host"], misskeyio_config["header"])
 
+    # DataFrameにparseする
     notelist = list()
     for note in tqdm(result2.json(), desc="Getting note..."):
         text = note["text"]
         noteid = note["id"]
-        notelist.append([text, noteid])
-    notelist = pd.DataFrame(notelist, columns=["text", "noteid"])
+        timestamp = note["createdAt"]
+        notelist.append([text, noteid, timestamp])
+    notelist = pd.DataFrame(notelist, columns=["text", "noteid", "timestamp"])
+    notelist["timestamp"] = pd.to_datetime(notelist["timestamp"]).dt.tz_convert("Asia/Tokyo")
+
+    # 保存していないもののみを抽出する
+    noteidlist = get_data("misskey.sqlite", "select distinct noteid from notelist")
+    notelist = notelist[~notelist["noteid"].isin(noteidlist["noteid"].tolist())]
     save_to_db("misskey.sqlite", notelist, "notelist", if_exists="append", index=False)
 
-    return notelist
+    # return notelist
 
 
-def get_reaction(notelist):
+def get_reaction():
     # 設定情報取得
     config = read_config("config.json")
     misskeyio_config = config["misskey.io"]
@@ -60,8 +68,13 @@ def get_reaction(notelist):
     reactionlist = list()
     endpoint = "notes/reactions"
 
-    for i, note in tqdm(notelist.iterrows(), total=len(notelist), desc="Getting reaction..."):
-        noteid = note["noteid"]
+    # 最新100件分のノートを取得する
+    noteidlist = get_data(
+        "misskey.sqlite",
+        "select distinct noteid from notelist order by timestamp desc limit 100"
+        )
+
+    for noteid in tqdm(noteidlist["noteid"].tolist(), desc="Getting reaction..."):
         params = {
             "i": misskeyio_config["token"],
             "noteId": noteid,
@@ -69,23 +82,51 @@ def get_reaction(notelist):
         response = get_result(endpoint, params, misskeyio_config["host"], misskeyio_config["header"])
         if len(response.json()) > 0:
             for reaction in response.json():
-                note = noteid
-                userId = reaction["user"]["id"]
+                userid = reaction["user"]["id"]
                 username = reaction["user"]["username"]
                 host = reaction["user"]["host"]
-                reactionlist.append([noteid, userId, username, host])
+                reactionlist.append([noteid, userid, username, host])
         sleep(0.05)
     reactionlist = pd.DataFrame(
         reactionlist,
         columns=["noteid", "userid", "username", "host"]
     ).fillna({"host": "misskey.io"})
 
-    save_to_db("misskey.sqlite", reactionlist, "reactionlist", if_exists="append", index=False)
+    # 保存していないリアクションのみを抽出する
+    reactionlist_all = get_data("misskey.sqlite", "select distinct noteid, userid from reactionlist")
+    reactionlist_all = reactionlist.merge(
+        reactionlist_all,
+        how="outer",
+        on=["noteid", "userid"],
+        indicator=True
+    )
+    reactionlist_all = reactionlist_all.query("_merge=='left_only'")[["noteid", "userid", "username", "host"]]
+    save_to_db("misskey.sqlite", reactionlist_all, "reactionlist", if_exists="append", index=False)
 
-    return reactionlist
 
-
-def following_user(reactionlist, th: int):
+def following_user(th: int):
+    # 直近1週間のノートのリアクション数を集計
+    now = datetime.datetime.now()
+    span = datetime.timedelta(days=7)
+    start_date = (now - span).strftime("%Y-%m-%d")
+    reactionlist = get_data(
+        "misskey.sqlite",
+        f"""
+        select
+            note.noteid,
+            note.timestamp,
+            note.text,
+            react.userid,
+            react.username,
+            react.host
+        from reactionlist as react
+        left join notelist as note
+        on note.noteid = react.noteid
+        where
+            timestamp >= '{start_date}'
+        order by timestamp
+        """
+    )
     # 設定情報取得
     config = read_config("config.json")
     misskeyio_config = config["misskey.io"]
@@ -120,10 +161,45 @@ def following_user(reactionlist, th: int):
         sleep(2)
 
 
+def add_users_into_list():
+    reaction_count = get_data(
+        "misskey.sqlite",
+        """
+            select
+                userid,
+                username,
+                count(noteid) as num
+            from reactionlist
+            where
+                userid not in ('7rkr3b1c1c', '82qrp4qp16')
+            group by userid, username
+            order by num desc
+        """
+    )
+    reaction_count["percent"] = reaction_count["num"] / reaction_count["num"].sum()
+
+    # 設定情報取得
+    config = read_config("config.json")
+    misskeyio_config = config["misskey.io"]
+    target_list_id = misskeyio_config["target_list_id"]
+
+    # リストに追加する
+    target_userid_list = reaction_count.query("percent >= 0.02")["userid"].tolist()
+    endpoint = "users/lists/push"
+    for userid in tqdm(target_userid_list, desc="Adding user into list..."):
+        params = {
+            "i": misskeyio_config["token"],
+            "userId": userid,
+            "listId": target_list_id
+        }
+        get_result(endpoint, params, misskeyio_config["host"], misskeyio_config["header"])
+
+
 def main(th=2):
-    notelist = get_note()
-    reactionlist = get_reaction(notelist)
-    following_user(reactionlist, th)
+    get_note()
+    get_reaction()
+    following_user(th)
+    add_users_into_list()
 
 
 schedule.every().saturday.at("13:00").do(main)
