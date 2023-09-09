c7cbf2bc3071b3d6aef2b05d230ad9b559fcfa5f
Laplusdestiny <prayonshootingstars@gmail.com>
2023-09-09 15:36:03 +0000

    Add information

--- connect_sqlite.py
+++ connect_sqlite.py
@@ -2,14 +2,17 @@
 
 import sqlite3
 import pandas as pd
+from logging import info
 
 
 def save_to_db(db_path, df: pd.DataFrame, table: str, **kwargs):
+    info(f"[connect_sqlite save_to_db] Save data to {db_path} table: {table}")
     with sqlite3.connect(db_path) as conn:
         df.to_sql(table, conn, **kwargs)
 
 
 def get_data(db_path, query):
+    info(f"[connect_sqlite get_data] Get data from {db_path} query: {query}")
     with sqlite3.connect(db_path) as conn:
         data = pd.read_sql_query(query, conn)
     return data
